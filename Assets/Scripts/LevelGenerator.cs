713e991448b3a0044f815cd1bd4460606abcbdc5
Stepan <kryzhankov.stepan@mail.ru>
2020-07-19 23:31:36 +0300

    Snow biome
    Ищменение цвета шара и дерева между зеленым и белым

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,16 +6,23 @@ public class LevelGenerator : MonoBehaviour
 {
     private float radius; 
     public GameObject[] objects;
- 
+    public GameObject Tree;
+    private List<GameObject> generatedObjects;
     private Vector3 center;
+    private float deltaChangeBiom; 
+    Color colorStart;
+    Color colorEnd = Color.white;
+    float duration = 10f;
     // Start is called before the first frame update
     void Start()
     {
+        // deltaChangeBiom = Random.Range(5f, 10f);
+        colorStart = GetComponent<Renderer>().material.color;
+        generatedObjects = new List<GameObject>();
         center = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         radius = 2.5f;
         for (int i = 0; i < 20; i++)
         {
-
             //GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
             GameObject cylinder = Instantiate(objects[Random.Range(0, 2)]);
             float theta = Mathf.Rad2Deg * (2 * Mathf.PI * Random.Range(0, 1f));
@@ -28,7 +35,14 @@ public class LevelGenerator : MonoBehaviour
             cylinder.transform.LookAt(center);
             cylinder.transform.Rotate(-90f, 0, 0);
             cylinder.transform.parent = transform;
+            generatedObjects.Add(cylinder);
         }
+    }
+
+    void Update() {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        GetComponent<Renderer>().material.color = Color.Lerp(colorStart, colorEnd, lerp);
+        Tree.GetComponent<Renderer>().materials[1].color = Color.Lerp(colorStart, colorEnd, lerp);
     }
 
 }
