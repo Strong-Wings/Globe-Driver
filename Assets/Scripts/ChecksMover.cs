ca3d6b7bb852d7c9b1ada2b6a3fd6baa898b4876
MaxFunProger <maxts2003@mail.ru>
2020-07-21 18:58:51 +0400

    Сделал скрипт паззла
    При сборе чека появуляется паззл. Паззл следует расположить в верхней части экрана и унаследовать от Main Camera. Сам чекпоинт нужно расположить на планете и унаследовать от сферы. В поле Puzzle нужно поместить паззл (не чекпоинт, а именно паззл).

---
+++ Assets/Scripts/ChecksMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ChecksMover : MonoBehaviour
+{
+
+    public Transform Puzzle;
+
+    void Start()
+    {
+        Puzzle.localScale *= 0;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(MoveCoroutine());
+            this.transform.localScale *= 0;
+        }
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        while (Puzzle.localScale != new Vector3(1, 1, 1))
+        {
+            Puzzle.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        this.gameObject.SetActive(false);
+    }
+}
